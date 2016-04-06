df6ef7b1f46c678915d2182cf5ce2b154f44c9a2
nick <shijh302@gmail.com>
2016-04-06 15:52:57 -0500

    change

---
+++ Client/wxchat/Controllers/ChatTableController.swift
@@ -0,0 +1,20 @@
+//
+//  ChatTableController.swift
+//  wxchat
+//
+//  Created by Junhua Shi on 4/6/16.
+//  Copyright © 2016 nick. All rights reserved.
+//
+
+
+import UIKit
+
+class ChatTableController: UITableViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("viewDidLoad")
+    }
+}
