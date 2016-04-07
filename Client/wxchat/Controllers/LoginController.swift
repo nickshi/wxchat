26027aa9d9e76731775b23901061fc8ff95db555
nick <shijh302@gmail.com>
2016-04-07 09:59:03 -0500

    login

---
+++ Client/wxchat/Controllers/LoginController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginController.swift
+//  wxchat
+//
+//  Created by nick.shi on 4/7/16.
+//  Copyright © 2016 nick. All rights reserved.
+//
+
+import UIKit
+
+
+class LoginController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+        
+        
+        
+    }
+    
+    
+    
+//    override func performSegueWithIdentifier(identifier: String, sender: AnyObject?) {
+//        
+//    }
+}
